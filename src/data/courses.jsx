41f178f7cd0494822cf705e829d3de0c613baf64
Losha Stanice <kieramaxswell@gmail.com>
2026-07-29 10:08:55 +0100

    Updating the name of my file and folder to data and courses

---
+++ src/data/courses.jsx
@@ -0,0 +1,37 @@
+const courses = [
+  {
+    id: 1,
+    title: "React Fundamentals",
+    instructor: "John Doe",
+    difficulty: "Beginner",
+    students: 250,
+    duration: "8 Hours",
+    lessons: 32,
+    image: "https://picsum.photos/300/200?random=1",
+    description: "Learn the fundamentals of React from scratch."
+  },
+  {
+    id: 2,
+    title: "JavaScript ES6",
+    instructor: "Jane Smith",
+    difficulty: "Intermediate",
+    students: 410,
+    duration: "10 Hours",
+    lessons: 45,
+    image: "https://picsum.photos/300/200?random=2",
+    description: "Master modern JavaScript features."
+  },
+  {
+    id: 3,
+    title: "HTML & CSS",
+    instructor: "Michael Brown",
+    difficulty: "Beginner",
+    students: 620,
+    duration: "6 Hours",
+    lessons: 25,
+    image: "https://picsum.photos/300/200?random=3",
+    description: "Build beautiful and responsive web pages."
+  }
+];
+
+export default courses;
