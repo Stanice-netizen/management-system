df83a1b6a0e469d1abc002c4f476b0a552f811cd
Losha Stanice <kieramaxswell@gmail.com>
2026-07-29 10:10:12 +0100

    A course management system

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,5 +0,0 @@
-function ErrorMessage({ message }) {
-  return <h2>{message}</h2>;
-}
-
-export default ErrorMessage;
--- src/Components/Loader/Loader.jsx
+++
@@ -1,5 +0,0 @@
-function Loader() {
-  return <h2>Loading courses...</h2>;
-}
-
-export default Loader;
