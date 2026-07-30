a432261786a0110ae3c9732ca9153b7678c65cb7
Losha Stanice <kieramaxswell@gmail.com>
2026-07-30 10:02:58 +0100

    Deleting the duplicate copy of my api file

--- src/services/Api.jsx
+++
@@ -1,9 +0,0 @@
-import courses from "../data/courses";
-
-export function getCourses() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(courses);
-    }, 1000);
-  });
-}
